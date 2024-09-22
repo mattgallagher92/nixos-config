50c01025138a15778b4c9af0762f4c13222e543d
Matt Gallagher <mjg1992@hotmail.co.uk>
2024-09-22 14:39:29 +0100

    Add neovim package

--- configuration.nix
+++ configuration.nix
@@ -100,7 +100,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     git
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    neovim
   #  wget
   ];
 
