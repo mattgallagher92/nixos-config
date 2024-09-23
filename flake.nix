034a328cbfb8950f204bdb037bf388308a0fce55
Matt Gallagher <mjg1992@hotmail.co.uk>
2024-09-23 09:49:53 +0100

    Use unstable packages

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Matt's NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
