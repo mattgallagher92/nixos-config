e04caf181806190cb1634601d365ceac6e16ae31
Matt Gallagher <you@example.com>
2025-01-27 08:43:29 +0000

    Enable rootless Docker

--- configuration.nix
+++ configuration.nix
@@ -100,6 +100,11 @@
     neovim
   ];
 
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
+  
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
